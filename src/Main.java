8bf6b3955876c30d6c38046be976a7ab5f2d8171
Ivan191 <zy3600924@gmail.com>
2023-10-07 00:12:55 +0800

    2023-10-07

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      PainterFrame123 application = new PainterFrame123();
+      application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      application.setSize(800, 600);
+      application.setVisible(true);
+   } 
+} 
